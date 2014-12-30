1eecebec452267025269c7653840ea0a8e82ecf4
houchanghong <houchanghong@me.com>
2014-12-30 11:40:11 +0800

    Initial Commit

---
+++ Tab/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tab
+//
+//  Created by Mac on 14/12/30.
+//  Copyright (c) 2014年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
